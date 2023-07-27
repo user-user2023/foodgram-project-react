a44a50a6c1a753c0d682a606d6a8e6b202ab0caa
Анатолий Левченко <ya.ru.practicum2023@yandex.ru>
2023-07-27 21:04:20 +0300

    edit

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -66,7 +66,7 @@ MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
