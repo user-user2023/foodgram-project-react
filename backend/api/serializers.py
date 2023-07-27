8809f8a9f0f16ac25c5221ed10494ecfb4368ac5
Анатолий Левченко <ya.ru.practicum2023@yandex.ru>
2023-07-27 16:04:55 +0300

    edit

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -91,7 +91,6 @@ class RecipeSerializer(serializers.ModelSerializer):
     author = MyUserSerializer(read_only=True)
     tags = TagsSerializer(read_only=True, many=True)
     ingredients = serializers.SerializerMethodField()
-    image = Base64ImageField(max_length=None)
 
     class Meta:
         model = Recipe
